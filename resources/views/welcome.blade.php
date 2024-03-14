e015807ce87249f5b850d66ee59db5e860bee3c3
wierdpanda <60147367+wierdpanda@users.noreply.github.com>
2024-03-15 00:14:31 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+<body class="bg-amber-600">
+    TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
+
+</body>
+</html>
